8b61acb92d38e211b240f23a5cc75350ff88b569
Marceau Lecomte <marceaul@users.noreply.github.com>
2024-07-23 11:30:35 +0200

    Update Package.swift

--- cmd/Package.swift
+++ cmd/Package.swift
@@ -9,14 +9,14 @@ let package = Package(
         SupportedPlatform.macOS(.v11)
     ],
     dependencies: [
-        .package(path: "/Users/marceaulecomte/Documents/GitHub/meemaw/client/ios/meemaw"), // development
-        // .package(url: "https://github.com/getmeemaw/meemaw-ios", from: "1.0.0"),
+        // .package(path: "/Users/marceaulecomte/Documents/GitHub/meemaw/client/ios/meemaw-ios"), // dev
+        .package(url: "https://github.com/getmeemaw/meemaw-ios", from: "1.2.0"),
         .package(url: "https://github.com/argentlabs/web3.swift", from: "1.1.0")
     ],
     targets: [
         .executableTarget(
             name: "cmd",
-            dependencies: ["meemaw", "web3.swift"]
+            dependencies: ["meemaw-ios", "web3.swift"]
         ),
     ]
 )
